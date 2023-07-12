850a719a4f40687ad5fd4a4a2f3b95cfe28f0395
ave6990 <ave6990@ya.ru>
2023-07-12 08:36:58 +0500

    On branch dev  Changes to be committed: 	renamed:    views/styles/main.css -> public/styles/main.css 	modified:   public/tasks.js 	modified:   views/index.pug 	modified:   views/tasks.pug

--- public/tasks.js
+++ public/tasks.js
@@ -1,7 +1,30 @@
+class Form {
+    constructor() {
+        this._form = document.getElementById('input_form')
+        this._inputs = Array.from(this._form.elements).filter(item => !!item.name)
+        console.log(this._inputs)
+    }
+
+    fill(data) {
+
+    }
+
+    clear() {
+
+    }
+
+    getData() {
+
+    }
+}
+
 const tab = document.getElementById('tasks_table')
+const form = new Form()
 
 tab.addEventListener('click', (ev) => {
     const row_num = ev.target.parentNode.id.split('_')[1]
     const id = document.getElementById(`cell_${row_num}_0`).innerHTML
     console.log(id)
 })
+
+
